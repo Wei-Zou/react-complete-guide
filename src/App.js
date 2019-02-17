0eef7369a2f3f9776744fb2739657b68df48bac3
Wei-Zou <wz.weizou@gmail.com>
2019-02-16 17:49:45 -0800

    [PATCH] lecture up to 45

--- src/App.js
+++ src/App.js
@@ -24,6 +24,16 @@ class App extends Component {
     });
   };
 
+  nameChangedHandler = (event) => {
+    this.setState({
+      persons: [
+        { name: 'Max', age: 28 },
+        { name: event.target.value, age: 29 },
+        { name: 'Stephanie', age: 27 }
+      ]
+    });
+  }
+
   render () {
     return (
       <div className="App">
@@ -36,7 +46,8 @@ class App extends Component {
         <Person
           name={this.state.persons[1].name}
           age={this.state.persons[1].age}
-          onClick={this.switchNameHandler.bind(this, 'Max!')}>
+          onClick={this.switchNameHandler.bind(this, 'Max!')}
+          onChange={this.nameChangedHandler}>
             My Hobbies: Racing
         </Person>
         <Person
