02a8611c9bc093721b4391cb76ce146fff4486e8
Wei-Zou <wz.weizou@gmail.com>
2019-02-17 20:12:11 -0800

    [PATCH] up to lecture 53

--- src/App.js
+++ src/App.js
@@ -53,21 +53,10 @@ class App extends Component {
     let persons = null;
     if (this.state.showPersons) {
       persons = (
-        <div>
+        this.state.persons.map((person) =>
           <Person
-            name={this.state.persons[0].name}
-            age={this.state.persons[0].age} />
-          <Person
-            name={this.state.persons[1].name}
-            age={this.state.persons[1].age}
-            onClick={this.switchNameHandler.bind(this, 'Max!')}
-            onChange={this.nameChangedHandler}>
-              My Hobbies: Racing
-          </Person>
-          <Person
-            name={this.state.persons[2].name}
-            age={this.state.persons[2].age} />
-        </div>
+            name={person.name}
+            age={person.age} />)
       );
     }
 
