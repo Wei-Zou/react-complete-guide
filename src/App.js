df011e15fe98921aa6a1021969ac0177eb2a83f9
Wei-Zou <wz.weizou@gmail.com>
2019-02-24 18:06:25 -0800

    [PATCH] remove Radium

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import Radium, { StyleRoot } from 'radium';
 
 import './App.css';
 import Person from './Person/Person';
@@ -59,10 +58,6 @@ class App extends Component {
       border: '1px solid blue',
       padding: '8px',
       cursor: 'pointer',
-      ':hover': {
-        backgroundColor: 'lightgreen',
-        color: 'black',
-      },
     };
 
     let persons = null;
@@ -81,10 +76,6 @@ class App extends Component {
         </div>
       );
       style.backgroundColor = 'red';
-      style[':hover'] = {
-        backgroundColor: 'salmon',
-        color: 'black',
-      };
     }
 
     const classes = [];
@@ -97,20 +88,18 @@ class App extends Component {
 
 
     return (
-      <StyleRoot>
-        <div className="App">
-          <h1>Hi, I'm a React App</h1>
-          <p className={classes.join(' ')}>This is really working!</p>
-          <button
-            style={style}
-            onClick={this.togglePersonsHandler}>
-            Toggle Persons
-          </button>
-          {persons}
-        </div>
-      </StyleRoot>
+      <div className="App">
+        <h1>Hi, I'm a React App</h1>
+        <p className={classes.join(' ')}>This is really working!</p>
+        <button
+          style={style}
+          onClick={this.togglePersonsHandler}>
+          Toggle Persons
+        </button>
+        {persons}
+      </div>
     );
   }
 };
 
-export default Radium(App);
+export default App;
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,22 +1,15 @@
 import React from 'react';
-import Radium from 'radium';
 
 import './Person.css';
 
 const person = (props) => {
-	const style = {
-		'@media (min-width: 500px)': {
-			width: '450px',
-		},
-	};
-
 	return (
-		<div className='Person' style={style}>
+	  <div className='Person'>
       <p onClick={props.onClick}>I'm {props.name} and I am {props.age} years old!</p>
-			<p>{props.children}</p>
+      <p>{props.children}</p>
       <input type="text" onChange={props.onChange} value={props.name} />
-		</div>
+	  </div>
 	);
 };
 
-export default Radium(person);
+export default person;
